74fee238c4581012b764338b89e538a646986c0d
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-22 11:55:03 +0600

    easy

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,48 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static void main(String[] args) {
+        System.out.println(getDataFromFile(new File("src/main/resources/Profile.txt")));
+    }
+
+    public static Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            StringBuilder s = new StringBuilder();
+            while ((ch = fileInputStream.read()) != -1) {
+                char x = (char) ch;
+                if (x == '\n') {
+                    String[] split = s.toString().trim().split(": ");
+                    switch (split[0]) {
+                        case "Name":
+                            profile.setName(split[1]);
+                            break;
+                        case "Age":
+                            profile.setAge(Integer.valueOf(split[1]));
+                            break;
+                        case "Email":
+                            profile.setEmail(split[1].trim());
+                            break;
+                        case "Phone":
+                            profile.setPhone(Long.valueOf(split[1].trim()));
+                            break;
+                    }
+                    s = new StringBuilder();
+                }
+                s.append(x);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
